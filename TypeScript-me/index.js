067e69f75b082998d044418b9367dec802138cc3
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-08-27 18:15:50 +0530

    add new files

---
+++ TypeScript-me/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// To RUN ... : - npx tsc -b  🏝️
+// 💡
+// Thing to learn - How to give types to arguments of a function
+function greet(firstName) {
+    console.log("Hello" + firstName);
+}
+greet("Khushiiii");
+greet("1");
---
+++ TypeScript-me/index.ts
@@ -0,0 +1,20 @@
+
+// To RUN ... : - npx tsc -b  🏝️
+
+// 💡 1
+// Thing to learn - How to give types to arguments of a function
+
+
+// function greet(firstName: any) {
+//     console.log("Hello" + firstName);
+
+// }
+
+// greet("Khushiiii");
+// greet("1");
+
+
+// 2 💚 -----------
+
+// Thing to learn - How to assign a return type to a function
+// Write a function that calculates the sum of two function
